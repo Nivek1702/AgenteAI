06915618a4f862329ce4e73d2631c353d1d8160e
Kevin <keviinpcg17@gmail.com>
2026-05-15 13:08:12 -0500

    prueba de conexion a neo4j Aura, correcta

---
+++ lib/neo4j.ts
@@ -0,0 +1,12 @@
+import neo4j from "neo4j-driver";
+
+const URI = process.env.NEO4J_URI!;
+const USER = process.env.NEO4J_USERNAME!;
+const PASSWORD = process.env.NEO4J_PASSWORD!;
+
+const driver = neo4j.driver(
+  URI,
+  neo4j.auth.basic(USER, PASSWORD)
+);
+
+export default driver;
